35d95991a831581c462210d79aa6da1916e51c45
enricoschaaf <enricoschaaf98@gmail.com>
2020-06-12 23:11:04 +0200

    netlify

--- pages/api/inbound/parse.ts
+++ pages/api/inbound/parse.ts
@@ -8,37 +8,35 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const form = new IncomingForm()
     const {
-      fields: { from, to, subject, html },
-      files
+      data: { from, to, subject, text, html },
+      attachments
     } = await new Promise((resolve, reject) => {
       form.parse(req, async (err, fields, files) => {
         if (err) reject(err)
-        resolve({ fields, files })
+        const [, attachments] = Object.entries(files)
+        resolve({ data: fields, attachments })
       })
     })
     const { id } = await prisma.email.create({
       data: {
-        from: typeof from === "string" && from,
-        to: typeof to === "string" && to,
+        from,
+        to,
         subject: typeof subject === "string" ? subject : "",
-        html: typeof html === "string" ? html : ""
+        html: typeof html === "string" ? html : text
       },
       select: { id: true }
     })
-    const [, attachments] = Object.entries(files)
-    if (attachments) {
-      await Promise.all(
-        attachments.map(([, file]) => {
-          prisma.attachment.create({
-            data: {
-              name: file.name,
-              url: "url",
-              Email: { connect: { id } }
-            }
-          })
+    await Promise.all(
+      attachments.map(([, file]) => {
+        prisma.attachment.create({
+          data: {
+            name: file.name,
+            url: "url",
+            Email: { connect: { id } }
+          }
         })
-      )
-    }
+      })
+    )
     res.end()
   }
   res.status(405).end()
